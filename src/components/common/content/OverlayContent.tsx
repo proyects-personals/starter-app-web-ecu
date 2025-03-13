aa1b3d418cde615458540744a93cc20e4790bab1
steveen ordoñez <zh311505@gmail.com>
2025-03-13 17:18:13 -0500

    feat: navegacion y estilos del home

--- src/components/common/content/OverlayContent.tsx
+++ src/components/common/content/OverlayContent.tsx
@@ -66,32 +66,61 @@ const OverlayContent: React.FC = () => {
 
   return (
     <div className="relative w-full p-8 flex flex-col items-center justify-center text-white space-y-14 mt-52 z-0">
-      <div className="text-4xl font-bold text-center">Bienvenido a Ecuador</div>
+      <div className="font-bold text-center space-y-6">
+        <h2 className="text-xl sm:text-3xl">Bienvenido a Ecuador</h2>
+        <h1 className="text-2xl sm:text-4xl">1 Destino, 4 Aventuras</h1>
+      </div>
       <div className="flex space-x-4">
-        <button className="px-6 py-2 bg-blue-600 rounded-lg hover:bg-blue-500 transition-colors">
-          Registro de comprador
+        <button className="text-sm sm:text-base px-6 py-2 bg-blue-600 rounded-lg hover:bg-blue-500 transition-colors">
+          Botón Call to Actión
         </button>
-        <button className="px-6 py-2 bg-blue-600 rounded-lg hover:bg-blue-500 transition-colors">
+        <button className="text-sm sm:text-base px-6 py-2 bg-blue-600 rounded-lg hover:bg-blue-500 transition-colors">
           Registro de vendedor
         </button>
       </div>
       <div>
-        <button className="px-6 py-2 bg-gray-500 rounded-lg hover:bg-green-500 transition-colors">
-          Ecuador,{date}
+        <button className="text-sm sm:text-base px-6 py-2 bg-gray-500 rounded-lg hover:bg-green-500 transition-colors">
+          Ecuador, {date}
         </button>
       </div>
-      <div className="grid grid-cols-4 gap-4">
+
+      <div className="grid grid-cols-4 gap-4 mt-8">
+        {/* Días */}
         <div className="bg-transparent p-4 text-center rounded-lg">
-          {timeLeft.days} Días
+          <div className="text-xl sm:text-6xl text-gray-300 sm:text-4xl">
+            {timeLeft.days}
+          </div>
+          <div className="text-sm sm:text-2xl text-gray-500">Días</div>
         </div>
+
+        {/* Hora */}
         <div className="bg-transparent p-4 text-center rounded-lg">
-          {timeLeft.hours} Horas
+          <div className="text-xl sm:text-6xl text-gray-300 sm:text-4xl">
+            {timeLeft.hours}
+          </div>
+          <div className="text-sm sm:text-2xl text-gray-500 sm:text-sm">
+            Horas
+          </div>
         </div>
+
+        {/* Minutos */}
         <div className="bg-transparent p-4 text-center rounded-lg">
-          {timeLeft.minutes} Min
+          <div className="text-xl sm:text-6xl text-gray-300 sm:text-4xl">
+            {timeLeft.minutes}
+          </div>
+          <div className="text-sm sm:text-2xl text-gray-500 sm:text-sm">
+            Minutos
+          </div>
         </div>
+
+        {/* Segundos */}
         <div className="bg-transparent p-4 text-center rounded-lg">
-          {timeLeft.seconds} Seg
+          <div className="text-xl sm:text-6xl text-gray-300 sm:text-4xl">
+            {timeLeft.seconds}
+          </div>
+          <div className="text-sm sm:text-2xl text-gray-500 sm:text-sm">
+            Segundos
+          </div>
         </div>
       </div>
     </div>
