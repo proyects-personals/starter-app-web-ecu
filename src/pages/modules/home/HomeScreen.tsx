7b02bd2a4c37a837fa7a0f40562a5e85a72c0653
steveen ordoñez <zh311505@gmail.com>
2025-03-18 09:49:41 -0500

    fix: video

--- src/pages/modules/home/HomeScreen.tsx
+++ src/pages/modules/home/HomeScreen.tsx
@@ -7,7 +7,7 @@ import imagePiquero from '../.././../assets/animals/PiqueroWEB.jpg';
 import imageTortuga from '../.././../assets/animals/TortugaWEB.jpg';
 import LocationInfo from '../../../components/home/LocationInfo';
 import Carousel from '../../../components/common/carrusels/Carousel';
-import videoHome from '../../../assets/videos/bgVideo.mp4';
+import videoHome from '../../../assets/animations/bgVideo.mp4';
 import halfOfTheWorld from '../../../assets/places/MitadMundoWEB.jpg';
 import Partners1Image from '../../../assets/logos/Partners1.png';
 
