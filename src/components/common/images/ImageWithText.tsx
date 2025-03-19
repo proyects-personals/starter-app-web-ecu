805de4098df95b75a4f5457f73d715755149f786
steveen ordoñez <zh311505@gmail.com>
2025-03-19 13:37:12 -0500

    feat: activities sections

--- src/components/common/images/ImageWithText.tsx
+++ src/components/common/images/ImageWithText.tsx
@@ -4,18 +4,27 @@ type ImageWithTextProps = {
   imageUrl: string;
   altText: string;
   text: string;
+  subtitle?: string;
 };
 
 const ImageWithText: React.FC<ImageWithTextProps> = ({
   imageUrl,
   altText,
   text,
+  subtitle,
 }) => {
   return (
     <div className="relative">
-      <img src={imageUrl} alt={altText} className="w-full h-80 object-cover" />
-      <div className="absolute top-0 left-0 w-full h-full flex items-end justify-center bg-black bg-opacity-50">
-        <span className="text-white text-4xl font-semibold mb-4">{text}</span>
+      <img src={imageUrl} alt={altText} className="w-full h-96 object-cover" />
+      <div className="absolute top-0 left-0 w-full h-full flex items-center justify-start bg-black bg-opacity-50 p-10">
+        <div className="mt-10 flex flex-col">
+          <span className="text-white text-4xl font-semibold">{text}</span>
+          {subtitle && (
+            <span className="text-white text-lg font-semibold mt-2">
+              {subtitle}
+            </span>
+          )}
+        </div>
       </div>
     </div>
   );
